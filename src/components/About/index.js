eaf25bb6e39e11c6ed0313f236d33aefb8314603
CrisGraciano <cristobalgraciano98@gmail.com>
2022-11-30 20:04:51 -0600

    about me updated

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About () {
+
+    return (
+        <section className="my-5" >
+            <h1 id="about">About Me</h1>
+            <div className="my-2">
+                {/* Add image of me here */}
+            <p>
+              My name is Cristobal Graciano and I live in San Antonio, Tx. I am currently a student in a Coding Bootcamp and am excited to learn Development and Software Engineering! This is my Portfolio made with React.js and has some of my Projects I have worked on.
+            </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
